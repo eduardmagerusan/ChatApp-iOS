2db770a45284d751b1a8f8d0105f65f8b9f05d3e
eduardmagerusan <eduard.magerusan@outlook.com>
2022-08-22 22:22:52 +0200

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Eduard Magerusan on 22.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
